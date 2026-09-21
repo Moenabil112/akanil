7e5487efdc11aa3b4db12d04b0b2a879bc6761d8
AKANIL <akanil.ceo@gmail.com>
2026-09-22 00:41:58 +0300

    qassas sprint0: add qassas-enterprise/apps/qassas-api/src/main.ts

---
+++ qassas-enterprise/apps/qassas-api/src/main.ts
@@ -0,0 +1,11 @@
+import "reflect-metadata";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api/v1");
+  await app.listen(Number(process.env.PORT ?? 3001), "0.0.0.0");
+}
+
+void bootstrap();
