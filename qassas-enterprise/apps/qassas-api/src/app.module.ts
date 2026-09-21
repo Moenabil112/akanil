ba63cc8db2ce067ba5883370e7931e25f01f4b7f
AKANIL <akanil.ceo@gmail.com>
2026-09-22 01:01:20 +0300

    qassas wp00c: wire IAM and target modules

--- qassas-enterprise/apps/qassas-api/src/app.module.ts
+++ qassas-enterprise/apps/qassas-api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "./auth/auth.module";
 import { DatabaseModule } from "./database/database.module";
 import { HealthController } from "./health/health.controller";
+import { TargetModule } from "./target/target.module";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule, TargetModule],
   controllers: [HealthController],
 })
 export class AppModule {}
