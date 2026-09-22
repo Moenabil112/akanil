efc100be54026b0ae9fbde5463a1f1114cce69a4
AKANIL <akanil.ceo@gmail.com>
2026-09-22 20:46:29 +0300

    qassas sprint3: add qassas-enterprise/apps/qassas-api/src/portfolio-intelligence/portfolio-intelligence.module.ts

---
+++ qassas-enterprise/apps/qassas-api/src/portfolio-intelligence/portfolio-intelligence.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module";
+import { PortfolioIntelligenceController } from "./portfolio-intelligence.controller";
+import { PortfolioIntelligenceService } from "./portfolio-intelligence.service";
+
+@Module({
+  imports: [AuthModule],
+  controllers: [PortfolioIntelligenceController],
+  providers: [PortfolioIntelligenceService],
+})
+export class PortfolioIntelligenceModule {}
