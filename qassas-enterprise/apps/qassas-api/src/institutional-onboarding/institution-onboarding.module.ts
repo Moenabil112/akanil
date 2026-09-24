dcb44db8e5e0a25cc4216938efcccb1fb194f674
AKANIL <akanil.ceo@gmail.com>
2026-09-24 16:57:09 +0300

    feat(m1): wire institutional admin activation workflow

--- qassas-enterprise/apps/qassas-api/src/institutional-onboarding/institution-onboarding.module.ts
+++ qassas-enterprise/apps/qassas-api/src/institutional-onboarding/institution-onboarding.module.ts
@@ -2,13 +2,26 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "../auth/auth.module";
 import { AuditModule } from "../audit/audit.module";
 import { DatabaseModule } from "../database/database.module";
+import { InstitutionAdminActivationClaimController } from "./institution-admin-activation-claim.controller";
+import { InstitutionAdminActivationController } from "./institution-admin-activation.controller";
+import { InstitutionAdminActivationService } from "./institution-admin-activation.service";
 import { InstitutionOnboardingController } from "./institution-onboarding.controller";
 import { InstitutionOnboardingService } from "./institution-onboarding.service";
 
 @Module({
   imports: [DatabaseModule, AuthModule, AuditModule],
-  controllers: [InstitutionOnboardingController],
-  providers: [InstitutionOnboardingService],
-  exports: [InstitutionOnboardingService],
+  controllers: [
+    InstitutionOnboardingController,
+    InstitutionAdminActivationController,
+    InstitutionAdminActivationClaimController,
+  ],
+  providers: [
+    InstitutionOnboardingService,
+    InstitutionAdminActivationService,
+  ],
+  exports: [
+    InstitutionOnboardingService,
+    InstitutionAdminActivationService,
+  ],
 })
 export class InstitutionOnboardingModule {}
