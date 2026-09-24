1084699d690694cea488cac690a46ce8ebe20bdc
AKANIL <akanil.ceo@gmail.com>
2026-09-24 14:41:10 +0300

    fix(o3): resolve auth guard dependencies in institutional modules

--- qassas-enterprise/apps/qassas-api/src/data-pipeline/data-pipeline.module.ts
+++ qassas-enterprise/apps/qassas-api/src/data-pipeline/data-pipeline.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module";
 import { DatabaseModule } from "../database/database.module";
 import { DataPipelineController } from "./data-pipeline.controller";
 import { DataPipelineService } from "./data-pipeline.service";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [DataPipelineController],
   providers: [DataPipelineService],
   exports: [DataPipelineService],
